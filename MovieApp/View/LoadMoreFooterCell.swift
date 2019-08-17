ea75b4a699f304a74c9a66d40463f5a27efbf81c
kashee ram kushwaha <kashee.nitd2016@gmail.com>
2019-08-17 23:17:16 +0530

    pagination is done

---
+++ MovieApp/View/LoadMoreFooterCell.swift
@@ -0,0 +1,13 @@
+//
+//  LoadMoreFooterCell.swift
+//  MovieApp
+//
+//  Created by Kashee ram on 8/17/19.
+//  Copyright © 2019 Kashee ram. All rights reserved.
+//
+
+import UIKit
+
+class LoadMoreFooterCell: CollectionBaseCell {
+    
+}
